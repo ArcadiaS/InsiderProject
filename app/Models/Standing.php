6a45a4631daf2c0f1a6ff291ba2badcea28e4d3e
Kağan KAHRAMAN <kaane.kahramane@gmail.com>
2023-06-12 18:18:05 +0300

    add relations

--- app/Models/Standing.php
+++ app/Models/Standing.php
@@ -4,12 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Standing extends Model
 {
     use HasFactory;
 
     protected $fillable = [
+        'league_id',
         'team_id',
         'played',
         'won',
@@ -20,7 +22,12 @@ class Standing extends Model
         'points',
     ];
 
-    public function team()
+    public function league(): BelongsTo
+    {
+        return $this->belongsTo(League::class);
+    }
+
+    public function team(): BelongsTo
     {
         return $this->belongsTo(Team::class);
     }
--- app/Models/Team.php
+++ app/Models/Team.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasManyThrough;
 use Illuminate\Database\Eloquent\Relations\MorphToMany;
 
@@ -28,12 +29,12 @@ class Team extends Model
         return $this->hasManyThrough(Season::class, League::class);
     }
 
-    public function home_competitions()
+    public function home_competitions(): HasMany
     {
         return $this->hasMany(Competition::class, 'home_team_id');
     }
 
-    public function away_competitions()
+    public function away_competitions(): HasMany
     {
         return $this->hasMany(Competition::class, 'away_team');
     }
